d29d8599fddcf670cc58992f12b8b08933ffd6b5
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-05 00:01:02 +0000

    add m&c test and index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,13 @@
 const request = require('request');
 
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`;
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) return callback(error, null);
+    const data = JSON.parse(body);
+    if (!data[0]) return callback('Breed not found!', null);
+    callback(null, data[0].description);
+  });
+};
 
-request(url, (error, response, body) => {
-  if (error) return console.log('error: ', error);
-  console.log('status code: ', response && response.statusCode);
-  const data = JSON.parse(body);
-  if (!data[0]) return console.log('Breed not found!');
-  console.log(data[0].description);
-});
+module.exports = {fetchBreedDescription};
